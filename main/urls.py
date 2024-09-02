7b93eac8b5ae83ef465b8ada9800bcc80671fdd4
DamaMichaelYohanna <get2dama11@gmail.com>
2024-09-03 00:25:56 +0100

    removed login and register url and view from main to account

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index_page'),
-    path('login', views.login_form, name='login_page'),
     path('dashboard', views.dashboard, name='dashboard_page'),
     path('form', views.dashboard_form_data, name='dashboard_form'),
     path('about', views.about, name='about-page'),
